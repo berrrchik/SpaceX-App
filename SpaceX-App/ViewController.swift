3da69ab113d3dc7b0460f2b0840a9e87c22614a2
berrrchik <berrrchik@mail.ru>
2025-07-24 14:13:37 +0300

    Initial Commit

---
+++ SpaceX-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX-App
+//
+//  Created by Анастасия Берчик on 7/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
