c3f01980cfc2a1ca594bed4a0a66983e18a7b45c
berrrchik <berrrchik@mail.ru>
2025-08-20 17:05:17 +0300

    refactor(launch): simplify LaunchView by removing data state management

--- SpaceX/Launch/View/LaunchView.swift
+++ SpaceX/Launch/View/LaunchView.swift
@@ -1,22 +1,65 @@
+//import UIKit
+//import SnapKit
+//
+//final class LaunchView: UIView {
+//    
+//    var launches: [LaunchElement]? {
+//        didSet {
+//            tableView.reloadData()
+//        }
+//    }
+//    
+//    var errorMessage: String? {
+//        didSet {
+//            tableView.reloadData()
+//        }
+//    }
+//    
+//    let tableView = UITableView()
+//    
+//    var onBackButtonTap: (() -> Void)?
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupView()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    private func setupView() {
+//        addSubview(tableView)
+//        setupTableView()
+//    }
+//    
+//    private func setupTableView() {
+//        addSubview(tableView)
+//        tableView.backgroundColor = .clear
+//        tableView.separatorStyle = .none
+//        tableView.register(LaunchCell.self, forCellReuseIdentifier: "LaunchCell")
+//        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = 120
+//        
+//        tableView.snp.makeConstraints { make in
+//            make.top.equalTo(safeAreaLayoutGuide).offset(40)
+//            make.leading.equalToSuperview().offset(32)
+//            make.trailing.equalToSuperview().inset(32)
+//            make.bottom.equalToSuperview()
+//        }
+//    }
+//    
+//    @objc private func backButtonTapped() {
+//        onBackButtonTap?()
+//    }
+//}
+
 import UIKit
 import SnapKit
 
 final class LaunchView: UIView {
     
-    var launches: [LaunchElement]? {
-        didSet {
-            tableView.reloadData()
-        }
-    }
-    
-    var errorMessage: String? {
-        didSet {
-            tableView.reloadData()
-        }
-    }
-    
     let tableView = UITableView()
-    
     var onBackButtonTap: (() -> Void)?
     
     override init(frame: CGRect) {
@@ -34,7 +77,6 @@ final class LaunchView: UIView {
     }
     
     private func setupTableView() {
-        addSubview(tableView)
         tableView.backgroundColor = .clear
         tableView.separatorStyle = .none
         tableView.register(LaunchCell.self, forCellReuseIdentifier: "LaunchCell")
