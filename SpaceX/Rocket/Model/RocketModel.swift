340fb0efc49e09a2953cc7bde4ed4c0527b99963
berrrchik <berrrchik@mail.ru>
2025-08-22 15:55:15 +0300

    feat(rocket): replace class with struct and rename burnTimeSEC to burnTimeSec

--- SpaceX/Rocket/Model/RocketModel.swift
+++ SpaceX/Rocket/Model/RocketModel.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-class RocketElement: Codable {
-    
+struct RocketElement: Codable {
     let height: MeasureSize
     let diameter: MeasureSize
     let mass: MeasureWeight
@@ -22,94 +21,40 @@ class RocketElement: Codable {
     
     var getPayloadWeight: (String, String) {
         let payloadWeightIsLb = UserDefaults.standard.bool(forKey: "payloadWeightIsLb")
-        
-        if payloadWeightIsLb {
-            return ("\(payloadWeights[0].lb)", "Полезная нагрузка, lb")
-        } else {
-            return ("\(payloadWeights[0].kg)", "Полезная нагрузка, kg")
-        }
+        return payloadWeightIsLb ? ("\(payloadWeights[0].lb)", "Полезная нагрузка, lb") : ("\(payloadWeights[0].kg)", "Полезная нагрузка, kg")
     }
     
     var getFirstFlightDate: String {
         let dateFormatterGet = DateFormatter()
         dateFormatterGet.dateFormat = "yyyy-MM-dd"
-        
         let dateFormatterPrint = DateFormatter()
         dateFormatterPrint.dateFormat = "dd MMM, yyyy"
-        
         if let date = dateFormatterGet.date(from: firstFlight) {
             return dateFormatterPrint.string(from: date)
         } else {
             return "-"
         }
     }
-    
-    enum CodingKeys: String, CodingKey {
-        case height, diameter, mass
-        case firstStage = "first_stage"
-        case secondStage = "second_stage"
-        case payloadWeights = "payload_weights"
-        case flickrImages = "flickr_images"
-        case name
-        case costPerLaunch = "cost_per_launch"
-        case firstFlight = "first_flight"
-        case country
-        case id
-    }
 }
 
-class MeasureSize: Codable {
+struct MeasureSize: Codable {
     let meters: Double?
     let feet: Double?
-    
-    init(meters: Double?, feet: Double?) {
-        self.meters = meters
-        self.feet = feet
-    }
 }
 
-class MeasureWeight: Codable {
+struct MeasureWeight: Codable {
     let kg: Int
     let lb: Int
-    
-    init(kg: Int, lb: Int) {
-        self.kg = kg
-        self.lb = lb
-    }
 }
 
-class FirstStage: Codable {
+struct FirstStage: Codable {
     let engines: Int
     let fuelAmountTons: Double
-    let burnTimeSEC: Int?
-    
-    enum CodingKeys: String, CodingKey {
-        case engines
-        case fuelAmountTons = "fuel_amount_tons"
-        case burnTimeSEC = "burn_time_sec"
-    }
-    
-    init(engines: Int, fuelAmountTons: Double, burnTimeSEC: Int?) {
-        self.engines = engines
-        self.fuelAmountTons = fuelAmountTons
-        self.burnTimeSEC = burnTimeSEC
-    }
+    let burnTimeSec: Int?
 }
 
-class SecondStage: Codable {
+struct SecondStage: Codable {
     let engines: Int
     let fuelAmountTons: Double
-    let burnTimeSEC: Int?
-    
-    enum CodingKeys: String, CodingKey {
-        case engines
-        case fuelAmountTons = "fuel_amount_tons"
-        case burnTimeSEC = "burn_time_sec"
-    }
-    
-    init(engines: Int, fuelAmountTons: Double, burnTimeSEC: Int?) {
-        self.engines = engines
-        self.fuelAmountTons = fuelAmountTons
-        self.burnTimeSEC = burnTimeSEC
-    }
+    let burnTimeSec: Int?
 }
