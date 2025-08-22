c6ec719ea97664264065a35316bba45472a69327
berrrchik <berrrchik@mail.ru>
2025-08-22 16:21:29 +0300

    feat: add constants for colors and fonts

---
+++ SpaceX/Supporting/Theme.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+struct AppColors {
+    // Основные цвета фона
+    static let black = UIColor.black
+    static let clear = UIColor.clear
+    static let darkGray = UIColor.darkGray  // Для imageContainer в RocketView
+    static let cardGray = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.00)  // Для cardView, square, grayRectangle
+    static let pageControlContainer = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha: 1.00)  // Для pageControlContainer в RocketViewController
+    static let buttonBackgroundGray = UIColor.gray.withAlphaComponent(0.3)  // Для showLaunchesButton в RocketView
+    
+    // Цвета текста и индикаторов
+    static let white = UIColor.white
+    static let lightGray = UIColor.lightGray  // Для pageControl.pageIndicatorTintColor
+    static let textGray56 = UIColor(red: 0.56, green: 0.56, blue: 0.56, alpha: 1.00)  // Для dateLabel, measureLabel, inactiveColor, error text
+    static let labelGray79 = UIColor(red: 0.79, green: 0.79, blue: 0.79, alpha: 1.00)  // Для label в RocketView (firstFlightLabel и т.д.)
+    static let activeBlack07 = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha: 1.00)  // Для activeColor в CustomToggleView
+    static let buttonTint96 = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)  // Для showLaunchesButton.tintColor
+}
+
+struct AppFonts {
+    // Семейство: LabGrotesque
+    static let regular14 = UIFont(name: "LabGrotesque-Regular", size: 14)!
+    static let regular16 = UIFont(name: "LabGrotesque-Regular", size: 16)!
+    static let regular20 = UIFont(name: "LabGrotesque-Regular", size: 20)!
+    
+    static let medium16 = UIFont(name: "LabGrotesque-Medium", size: 16)!
+    
+    static let bold14 = UIFont(name: "LabGrotesque-Bold", size: 14)!
+    static let bold16 = UIFont(name: "LabGrotesque-Bold", size: 16)!
+    static let bold18 = UIFont(name: "LabGrotesque-Bold", size: 18)!
+    static let bold32 = UIFont(name: "LabGrotesque-Bold", size: 32)!
+}
