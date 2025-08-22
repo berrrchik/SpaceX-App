2215a6e8f20f92646b26b4faa7c819d9c785e5c4
berrrchik <berrrchik@mail.ru>
2025-08-22 15:53:16 +0300

    feat(services): add keyDecodingStrategy.convertFromSnakeCase to LaunchService

--- SpaceX/Launch/Service/LaunchService.swift
+++ SpaceX/Launch/Service/LaunchService.swift
@@ -4,9 +4,12 @@ import Foundation
 class LaunchService: LaunchServiceProtocol {
     private let endpoint = "https://api.spacexdata.com/v4/launches"
     func fetchLaunch(completion: @escaping (Result<[LaunchElement], any Error>) -> Void) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
         AF.request(endpoint)
             .validate()
-            .responseDecodable(of: [LaunchElement].self) { response in
+            .responseDecodable(of: [LaunchElement].self, decoder: decoder) { response in
                 switch response.result {
                 case .success(let launch):
                     completion(.success(launch))
