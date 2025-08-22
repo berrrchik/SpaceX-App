8b0166bd0a19f4a0e3a82a43a1801b97bfad532e
berrrchik <berrrchik@mail.ru>
2025-08-22 15:52:50 +0300

    feat(launch): replace class with struct and rename dateUTC to dateUtc

--- SpaceX/Launch/Model/LaunchModel.swift
+++ SpaceX/Launch/Model/LaunchModel.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-class LaunchElement: Codable {
+struct LaunchElement: Codable {
     let name: String
-    let dateUTC: String
+    let dateUtc: String
     let success: Bool?
     let rocket: String
     
@@ -13,44 +13,20 @@ class LaunchElement: Codable {
         let dateFormatterPrint = DateFormatter()
         dateFormatterPrint.dateFormat = "dd MMM, yyyy"
         
-        if let date = dateFormatterGet.date(from: String(dateUTC.prefix(10))) {
+        if let date = dateFormatterGet.date(from: String(dateUtc.prefix(10))) {
             return dateFormatterPrint.string(from: date)
         } else {
             return "-"
         }
     }
-    
-    enum CodingKeys: String, CodingKey {
-        case name
-        case dateUTC = "date_utc"
-        case success
-        case rocket
-    }
-    
-    init(name: String, dateUTC: String, success: Bool?, rocket: String) {
-        self.name = name
-        self.dateUTC = dateUTC
-        self.success = success
-        self.rocket = rocket
-    }
 }
 
-class RocketLaunch: Codable {
+struct RocketLaunch: Codable {
     let id: String
     let name: String
-    
-    init(id: String, name: String) {
-        self.id = id
-        self.name = name
-    }
 }
 
-class RocketWithLaunches {
+struct RocketWithLaunches {
     let rocket: RocketLaunch
     var launches: [LaunchElement]
-    
-    init(rocket: RocketLaunch, launches: [LaunchElement]) {
-        self.rocket = rocket
-        self.launches = launches
-    }
 }
