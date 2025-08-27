691046613415b0040ed85c5d54ddfc2857570e18
berrrchik <berrrchik@mail.ru>
2025-08-27 18:26:48 +0300

    refactor(launch): move date formatting to String extension

--- SpaceX/Launch/Model/LaunchModel.swift
+++ SpaceX/Launch/Model/LaunchModel.swift
@@ -7,16 +7,6 @@ struct LaunchElement: Codable {
     let rocket: String
     
     var getDateUTC: String {
-        let dateFormatterGet = DateFormatter()
-        dateFormatterGet.dateFormat = "yyyy-MM-dd"
-        
-        let dateFormatterPrint = DateFormatter()
-        dateFormatterPrint.dateFormat = "dd MMM, yyyy"
-        
-        if let date = dateFormatterGet.date(from: String(dateUtc.prefix(10))) {
-            return dateFormatterPrint.string(from: date)
-        } else {
-            return "-"
-        }
+        return dateUtc.formatDate() ?? "-"
     }
 }
