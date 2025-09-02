d1b3458dc8f37beb48516bc4467665761af9b592
berrrchik <berrrchik@mail.ru>
2025-09-02 11:23:14 +0300

    feat(AppFonts): add custom font

--- SpaceX/Supporting/AppFonts.swift
+++ SpaceX/Supporting/AppFonts.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 struct AppFonts {
-    // Семейство: LabGrotesque
     static let regular14 = UIFont(name: "LabGrotesque-Regular", size: 14)!
     static let regular16 = UIFont(name: "LabGrotesque-Regular", size: 16)!
     static let regular20 = UIFont(name: "LabGrotesque-Regular", size: 20)!
@@ -12,4 +11,5 @@ struct AppFonts {
     static let bold16 = UIFont(name: "LabGrotesque-Bold", size: 16)!
     static let bold18 = UIFont(name: "LabGrotesque-Bold", size: 18)!
     static let bold32 = UIFont(name: "LabGrotesque-Bold", size: 32)!
+    static let bold64 = UIFont(name: "LabGrotesque-Bold", size: 64)!
 }
